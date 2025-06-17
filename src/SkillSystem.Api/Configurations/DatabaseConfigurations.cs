afbd0be3d26e553d570df13a935922510bd039d4
user <user@DESKTOP-E0S9FSI>
2025-06-17 14:29:22 +0500

    dbconfiguration extention method added

---
+++ src/SkillSystem.Api/Configurations/DatabaseConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkillSystem.Infrastructura.Persistence;
+using System;
+
+namespace SkillSystem.Api.Configurations
+{
+    public static class DatabaseConfigurations
+    {
+        public static void ConfigureDatabase(this WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("DatabaseConnection");
+            builder.Services.AddDbContext<MainContext>(options => options.UseSqlServer(connectionString));
+        }
+    }
+}
