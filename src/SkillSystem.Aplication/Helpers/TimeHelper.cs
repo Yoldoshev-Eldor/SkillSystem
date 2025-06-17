1b4b58d043b6f51e979cbde40f32de40c3c427c8
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-06-17 15:40:31 +0500

    token service were added

---
+++ src/SkillSystem.Aplication/Helpers/TimeHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace SkillSystem.Aplication.Helpers;
+
+public static class TimeHelper
+{
+    public static DateTime GetDateTime()
+    {
+        var dtTime = DateTime.UtcNow;
+        dtTime.AddHours(5);
+        return dtTime;
+    }
+}
