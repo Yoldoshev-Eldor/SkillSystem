e86d1598871c020c43f7582712b5735b3f652d7d
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-06-17 13:53:09 +0500

    Fleunt Api configurations complited

---
+++ src/SkillSystem.Infrastructura/Persistence/Configurations/SkillConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using SkillSystem.Domain.Entities;
+
+namespace SkillSystem.Infrastructura.Persistence.Configurations;
+
+public class SkillConfiguration : IEntityTypeConfiguration<Skill>
+{
+    public void Configure(EntityTypeBuilder<Skill> builder)
+    {
+        builder.HasKey(s => s.SkillId);
+
+        builder.Property(s => s.Name).IsRequired().HasMaxLength(100);
+        builder.Property(s => s.Description).HasMaxLength(500);
+        builder.Property(s => s.Level).IsRequired();
+        builder.Property(s => s.CreatedAt).IsRequired();
+        builder.Property(s => s.UpdatedAt);
+
+        builder.HasOne(s => s.User)
+               .WithMany(u => u.Skills)
+               .HasForeignKey(s => s.UserId)
+               .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ src/SkillSystem.Infrastructura/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SkillSystem.Domain.Entities;
+
+namespace SkillSystem.Infrastructura.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.UserId);
+
+        builder.Property(u => u.FirstName).IsRequired().HasMaxLength(100);
+        builder.Property(u => u.LastName).IsRequired().HasMaxLength(100);
+        builder.Property(u => u.UserName).IsRequired().HasMaxLength(100);
+        builder.Property(u => u.Email).IsRequired().HasMaxLength(255);
+        builder.Property(u => u.Password).IsRequired();
+        builder.Property(u => u.PhoneNumber).HasMaxLength(50);
+        builder.Property(u => u.Salt).IsRequired();
+        builder.Property(u => u.Role).IsRequired();
+
+        builder.HasMany(u => u.Skills)
+               .WithOne(s => s.User)
+               .HasForeignKey(s => s.UserId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(u => u.RefreshTokens)
+               .WithOne(rt => rt.User)
+               .HasForeignKey(rt => rt.UserId)
+               .OnDelete(DeleteBehavior.Cascade);
+    }
+}
