ba32a53b1da19650b7e64e50e211ff7b930f4798
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-06-17 14:38:07 +0500

    appsettings json file fixed

--- src/SkillSystem.Api/Program.cs
+++ src/SkillSystem.Api/Program.cs
@@ -3,7 +3,7 @@ using SkillSystem.Api.Configurations;
 
 namespace SkillSystem.Api
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
