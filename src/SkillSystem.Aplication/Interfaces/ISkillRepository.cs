e710ecc2cd691ecc089748a0ee4b2ca16e4afe04
Sherlock <shholmes359@gmail.com>
2025-06-17 18:01:48 +0500

    skill interface created

---
+++ src/SkillSystem.Aplication/Interfaces/ISkillRepository.cs
@@ -0,0 +1,12 @@
+﻿using SkillSystem.Domain.Entities;
+
+namespace SkillSystem.Aplication.Interfaces;
+
+public interface ISkillRepository
+{
+    Task<long> InsertAsync(Skill skill);
+    Task DeleteByIdAsync(long skillId);
+    Task<Skill> SelectByIdAsync(long skillId);
+    Task<ICollection<Skill>> SelectAll();
+    Task UpdateAsync(Skill skill);
+}
