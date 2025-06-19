c18f59aa99a18ca85d6b141a71c69d853c2ff489
Nomozov <kamoliddinnomozov2606@gmail.com>
2025-06-19 14:44:24 +0500

    Add SkillSystem.Core layer

---
+++ src/SkillSystem.Core/Errors/ForbiddenException.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Runtime.Serialization;
+
+
+namespace SkillSystem.Core.Errors;
+
+public class ForbiddenException : BaseException
+{
+    public ForbiddenException() { }
+    public ForbiddenException(String message) : base(message) { }
+    public ForbiddenException(String message, Exception inner) : base(message, inner) { }
+    protected ForbiddenException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+}
