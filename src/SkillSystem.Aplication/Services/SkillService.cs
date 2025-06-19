85f4b0d0960e950e95f89ed97532557db10a19f3
Nomozov <kamoliddinnomozov2606@gmail.com>
2025-06-19 14:55:28 +0500

    implement interface

--- src/SkillSystem.Aplication/Services/SkillService.cs
+++ src/SkillSystem.Aplication/Services/SkillService.cs
@@ -1,5 +1,31 @@
-﻿namespace SkillSystem.Aplication.Services;
+﻿using SkillSystem.Aplication.Dtos;
+
+namespace SkillSystem.Aplication.Services;
 
 public class SkillService : ISkillService
 {
+    public Task DeleteAsync(long skillId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ICollection<SkillGetDto> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<SkillGetDto> GetByIdAsync(long skillId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<long> PostAsync(SkillCreateDto skillCreateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(SkillUpdateDto skillUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
 }
