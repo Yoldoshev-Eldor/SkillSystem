2e3e38cfc029a7c64dc94aa1b1ebd6355e9437a4
Komiljon154 <komiljonk154@gmail.com>
2025-06-20 23:08:11 +0500

    Create UserService and IUserService implemented

---
+++ src/SkillSystem.Aplication/Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using SkillSystem.Aplication.Dtos;
+using SkillSystem.Domain.Entities;
+
+namespace SkillSystem.Aplication.Services;
+
+public class UserService : IUserService
+{
+    public Task<long> CreateAsync(UserCreateDto userCreateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ICollection<UserGetDto> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PagedResult<UserGetDto>> GetAllAsync(int skip, int take)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserGetDto> GetByIdAsync(long userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(UserUpdateDto userUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+}
