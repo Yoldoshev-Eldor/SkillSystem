91fa503ef19c048ebe4d37b18a5cf135f5d00682
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-06-20 23:07:42 +0500

    refreshtoke repository has been added

---
+++ src/SkillSystem.Aplication/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,11 @@
+﻿using SkillSystem.Domain.Entities;
+
+namespace SkillSystem.Aplication.Interfaces;
+
+public interface IRefreshTokenRepository
+{
+    Task InsertRefreshTokenAsync(RefreshToken refreshToken);
+    Task<RefreshToken> SelectRefreshTokenAsync(string refreshToken, long userId);
+    Task<RefreshToken?> SelectActiveTokenByUserIdAsync(long userId);
+    Task RemoveRefreshTokenAsync(string token);
+}
