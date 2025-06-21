aca8b43ffa8a4faf88f35a896be84d08a061b241
Nomozov <kamoliddinnomozov2606@gmail.com>
2025-06-21 19:16:52 +0500

    add RoleConfigurations

---
+++ src/SkillSystem.Infrastructura/Persistence/Configurations/RoleConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SkillSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillSystem.Infrastructura.Persistence.Configurations;
+
+public class RoleConfiguration : IEntityTypeConfiguration<UserRole>
+{
+    public void Configure(EntityTypeBuilder<UserRole> builder)
+    {
+        builder.ToTable("Roles");
+        builder.HasKey(r => r.RoleId);
+        builder.Property(r => r.RoleName)
+            .IsRequired()
+            .HasMaxLength(50);
+        builder.Property(r => r.Description)
+            .IsRequired(false)
+            .HasMaxLength(255);
+        builder.HasMany(r => r.Users)
+            .WithOne(u => u.UserRole)
+            .HasForeignKey(u => u.UserRole)
+            .OnDelete(DeleteBehavior.Cascade);
+
+    }
+}
+
+
